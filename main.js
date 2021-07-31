16268bbbfa05b9ee1e15b0b8fe7ebda7a81ec06c
Harunur5252 <hroshid695@gmil.com>
2021-07-31 20:33:19 +0600

    code refactored using class(es6) feature

--- main.js
+++ main.js
@@ -1,33 +1,58 @@
-const numberElm = document.querySelector('.number')
-const decreaseBtnElm = document.querySelector('.dBtn')
-const resetBtnElm = document.querySelector('.rBtn')
-const increaseBtnElm = document.querySelector('.iBtn')
-
-let increase = 0
-increaseBtnElm.addEventListener('click',()=>{
-    increase++
-    numberElm.textContent=increase
-    if(increase===0){
-        numberElm.style.color='black'
-    }else{
-        numberElm.style.color='green'
-    }
-})
-
-decreaseBtnElm.addEventListener('click',()=>{
-    increase--
-    numberElm.textContent=increase
-   if(increase<0){
-     numberElm.style.color='red'
-   }
-   if(increase===0){
-    numberElm.style.color='black'
-  }
-})
-
-resetBtnElm.addEventListener('click',()=>{
+class Data{
     increase = 0
-    numberElm.textContent=increase
-    numberElm.style.color='black'
-})
+}
+const data = new Data()
+
+class UI{
+    selectors(){
+        const numberElm = document.querySelector('.number')
+        const decreaseBtnElm = document.querySelector('.dBtn')
+        const resetBtnElm = document.querySelector('.rBtn')
+        const increaseBtnElm = document.querySelector('.iBtn')
+        return {
+            numberElm,decreaseBtnElm,resetBtnElm,increaseBtnElm
+        }
+    }
+    getIncreaseValue(){
+        const {numberElm} = this.selectors()
+        data.increase++
+        // console.log(data.increase)
+        numberElm.textContent=data.increase
+        if(data.increase===0){
+            numberElm.style.color='black'
+        }else if(data.increase>0){
+            numberElm.style.color='green'
+        }
+        else if(data.increase<0){
+            numberElm.style.color='red'
+        }
+    }
+    getDecreaseValue(){
+        const {numberElm} = this.selectors()
+        data.increase--
+        numberElm.textContent=data.increase
+       if(data.increase<0){
+         numberElm.style.color='red'
+       }
+       if(data.increase===0){
+        numberElm.style.color='black'
+      }
+    }
+    resetValue(){
+        const {numberElm} = this.selectors()
+        data.increase = 0
+        numberElm.textContent=data.increase
+        numberElm.style.color='black'
+    }
+    loadAllEventListener(){
+        const {increaseBtnElm,decreaseBtnElm,resetBtnElm} = this.selectors()
+        increaseBtnElm.addEventListener('click',()=>this.getIncreaseValue())
+        decreaseBtnElm.addEventListener('click',()=>this.getDecreaseValue())
+        resetBtnElm.addEventListener('click',()=>this.resetValue())
+    }
+}
+const ui = new UI()
+
+ui.loadAllEventListener()
+
 
